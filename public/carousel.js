14cf1e256e9d613f5895370461da2e6b92e92156
samithseu <samithseu2002@gmail.com>
2025-10-31 02:26:54 +0700

    [ui]: added carousel for news card

--- public/carousel.js
+++ public/carousel.js
@@ -3,20 +3,43 @@ document.addEventListener("alpine:init", () => {
     emblaApi: null,
     canScrollPrev: false,
     canScrollNext: false,
+    scrollSnaps: [],
+    selectedIndex: 0,
 
     // initialization
     init() {
-      this.emblaApi = EmblaCarousel(this.$refs.emblaNode, {
-        watchDrag: false,
-        breakpoints: {
-          "(width >= 40rem)": { slidesToScroll: 1 },
-          "(width > 48rem)": { slidesToScroll: 2 },
-          "(width > 64rem)": { slidesToScroll: 3 },
-        },
+      this.$nextTick(() => {
+        this.emblaApi = EmblaCarousel(this.$refs.emblaNode, {
+          watchDrag: false,
+          slidesToScroll: 1,
+          breakpoints: {
+            "(min-width: 48rem)": { slidesToScroll: 2 },
+            "(min-width: 64rem)": { slidesToScroll: 3 },
+          },
+        });
+
+        // scroll statuses
+        this.updateScrollStatus();
+        this.emblaApi
+          .on("scroll", this.updateScrollStatus.bind(this))
+          .on("reInit", this.updateScrollStatus.bind(this));
+
+        // dot statuses
+        this.updateDotState();
+        this.emblaApi
+          .on("select", this.updateDotState.bind(this))
+          .on("reInit", this.updateDotState.bind(this));
       });
-      this.updateScrollStatus();
-      this.emblaApi.on("scroll", this.updateScrollStatus.bind(this));
-      this.emblaApi.on("reInit", this.updateScrollStatus.bind(this));
+    },
+
+    // update dot statuses
+    updateDotState() {
+      this.scrollSnaps = this.emblaApi.scrollSnapList();
+      this.selectedIndex = this.emblaApi.selectedScrollSnap();
+    },
+
+    scrollTo(index) {
+      this.emblaApi.scrollTo(index);
     },
 
     // update scroll status
@@ -26,13 +49,13 @@ document.addEventListener("alpine:init", () => {
         this.canScrollNext = this.emblaApi.canScrollNext();
       }
     },
-
+    // scroll previous
     scrollPrev() {
       if (this.emblaApi) {
         this.emblaApi.scrollPrev();
       }
     },
-
+    // scroll next
     scrollNext() {
       if (this.emblaApi) {
         this.emblaApi.scrollNext();
